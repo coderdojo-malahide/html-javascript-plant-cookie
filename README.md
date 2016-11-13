71673355223d5b867a26dcdab020084f6b7a9520
Tony Haddon <tony.haddon@gmail.com>
2016-11-13 15:27:50 +0000

    Some improvements and extra comments made to explain each step in the javascript.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # html-javascript-plant-cookie
-How to plant a cookie and test for it — allowing different outcomes on a html page depending on whether the cookie is there or not
+How to plant a cookie and test for it — allowing different outcomes on a html page depending on whether the cookie is there or not.
+
+You'll need to use a local web server such as [Mongoose](https://www.cesanta.com/products/binary), and set the configuration to point the document_root to this repo folder on your computer.
+
+When you have planted the cookie, you can see where it's kept in the browser. Using google chrome, you can go to (chrome://settings/cookies) and search for localhost. Other browsers will have different ways to view it.
--- index.js
+++ index.js
@@ -4,13 +4,25 @@
 
 function init() {
 	/*
-		This function adds the onclick behaviour to the button with the 'id="plant-cookie-button"'
+		This function tells the page what to do when the button is clicked.
+		The id on the button is "plant-cookie-button". Check the id on the
+		button in the html file.
+
+		We'll use the id to identify the button to the page.
 	*/
 
-	// Loads the button into the theButton variable, so that we can use it later.
+	/* 
+		This line uses the document.getElementById() method to load the button
+		into the theButton variable, so that we can use it in JavaScript.
+	*/
 	var theButton = document.getElementById("plant-cookie-button");
 
-	// Uses an onclick
+	
+	/*
+		Here's where we use that variable. We use the addEventListener() method
+		(set to a "click" type) to launch the addCookie function.
+	*/
+
 	theButton.addEventListener("click",addCookie);
 
 }
@@ -26,9 +38,32 @@ function addCookie() {
     document.cookie = "mycookie=1; path=/";
 }
 
-function getCookie(c_name) {
+function getCookie() {
+
+	/*
+		We use document.cookie.length to check if there are any cookies for our domain.
+		The domain is the name for your website.
+		Google.com is the domain of that website.
+		On your local server, the domain is called "localhost".
+	*/
+
     if (document.cookie.length > 0) {
+
+    	/*
+			We are using document.getElementById() again to get the p tag
+			that we have in the html that has an id of "result".
+
+			We are assigning the object that we get to a varable called 
+			"resultDisplay".
+    	*/
+
 		var resultDisplay = document.getElementById("result");
+
+		/*
+			Then we set a property of the resultDisplay object as the 
+			text we want to show when we've found a cookie.
+		*/
+
 		resultDisplay.innerHTML = "Cookie found! You can do something different on this page."
     }
 }
